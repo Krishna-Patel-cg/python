f4ca92b3ba2cc09d7c9affa3109082efb2678e9d
Krishna-Patel-cg <krishna.p.patel.cg@gmail.com>
2026-08-24 22:10:46 +0530

    Data type assignment question

---
+++ Data type/Q24.py
@@ -0,0 +1,6 @@
+student_name="Krishna Patel"
+student_age=19
+student_height=5.4
+city="varanasi"
+result=None
+print(type(student_name),type(student_age),type(student_height),type(city),type(result))
---
+++ Data type/Q30.py
@@ -0,0 +1,8 @@
+a = 10
+b=10.0
+c="10"
+d=True
+e="True"
+f=None
+g="None"
+print(type(a),type(b),type(c),type(d),type(e),type(f))
