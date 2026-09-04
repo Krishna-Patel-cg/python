0564e60059b5026989850c037505d08d2673022e
Krishna-Patel-cg <krishna.p.patel.cg@gmail.com>
2026-09-04 09:37:31 +0530

    boolean folder added

---
+++ 06input_output/class_question.py
@@ -0,0 +1,40 @@
+# name=input("Enter your name")
+# lab=input("Lab name")
+# product_name=input("projuct name")
+# product_quantity=int(input("enter quantity"))
+# price=int(input("enter price"))
+# total=product_quantity*price
+# print(f"My name is:,{name} study in:,{lab} I am eating:,{product_name}  number of mango quantity:,{product_quantity}  price:,{price}")
+# print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a,b=map(int,input("Enter two number:").split())
+# print(a,b, type(a), type(b))
+# print(a+b)
+#output=500 int int
+
+# a,b=map(bool,input("Enter two number:").split())
+# print(a,b, type(a), type(b))
+# print(a+b)
+# output=True True a+b=2 bool bool
+# a,b=map(str,input("Enter two number:").split())
+# print(a,b, type(a), type(b))
+# print(a+b)
+# output=300200 str str
+
+
+# a,b=map(None,input("Enter two number:").split())
+# print(a,b, type(a), type(b))
+# print(a+b)
