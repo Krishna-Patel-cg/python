cd94654b11fb3d745940787389ca93166c825ae0
Krishna-Patel-cg <krishna.p.patel.cg@gmail.com>
2026-09-04 09:35:30 +0530

    data folder added

---
+++ 03Data type/Q28.py
@@ -0,0 +1,6 @@
+age=19
+print(type(age))
+age="19"
+print(type(age))
+age=bool(19)
+print(type(age))
---
+++ 03Data type/Q29.py
@@ -0,0 +1,6 @@
+name="Krishna"
+age=19
+height=5.4
+information=True
+result=None
+print(type(name),type(age),type(height),type(information),type(result))
