fca555b9d0cbf220f2ed04c89d78e3f65ee6de07
Krishna-Patel-cg <krishna.p.patel.cg@gmail.com>
2026-09-06 21:11:47 +0530

    class file added

---
+++ 07conditional statment/classquestion.py
@@ -0,0 +1,26 @@
+# num=12
+# if num%2==0 :
+#     print("Even number")
+
+#     if num%2==1 :
+#         print("Odd number")
+
+# name=input("enter your name")
+
+# if name=="krishna":
+#     age=int(input("Enter your name: "))
+#     if age>=18:
+#         city=input("eneter yuor city name: ")
+#         if city=="varanasi":
+#             mobile=input("enter mobile number: ")
+#             if mobile=="7398924004":
+#                 print("student profile detail right")
+# print("18"==18)
+# print(int("18")>=18)
+# name=input("Enter you name").upper().strip()
+# if name  =="krishna":
+#     print("your document all match")
+# else :
+#     print("Unsuccesful varification")
+indian=input("are you indian? Yes or No")
+     
