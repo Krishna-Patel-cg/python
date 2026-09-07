fe156d3f1e66b36c0f0d2227d2809f61dd73f62b
Krishna-Patel-cg <krishna.p.patel.cg@gmail.com>
2026-09-07 23:23:52 +0530

    q13 added

---
+++ 07conditional statment/Q13.py
@@ -0,0 +1,13 @@
+days=int(input("Enter days"))
+if days==1:
+    print("Monday")
+elif days==2:
+    print("Tuesday")
+elif days==3:
+    print("Wednesday")
+elif days==4:
+    print("Thusday")
+elif days==5:
+    print("Friday")
+else:
+    print("Other")
