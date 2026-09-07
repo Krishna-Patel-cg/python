ac5e842f7b268dc94835a2b1da9dc67c389786ee
Krishna-Patel-cg <krishna.p.patel.cg@gmail.com>
2026-09-07 23:24:19 +0530

    q14 added

---
+++ 07conditional statment/Q14.py
@@ -0,0 +1,9 @@
+marks=int(input("Enter your name"))
+if marks>=90 and marks<=100:
+    print("Axcellent")
+elif marks>=89 and marks<=75:
+    print("Good")
+elif marks>=74 and marks<=34:
+    print("Pass")
+elif marks<=33:
+    print("Fail")
