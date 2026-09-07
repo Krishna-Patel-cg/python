dd8eb315133461f895cf88d532178d231933531f
Krishna-Patel-cg <krishna.p.patel.cg@gmail.com>
2026-09-07 23:22:58 +0530

    q11 added

---
+++ 07conditional statment/Q11.py
@@ -0,0 +1,16 @@
+# n=input("name")
+# if n=="Kri":
+#     print("dnisjd")
+# elif n=="Krishna":
+#     print("Krishna")
+marks=int(input("Enter your name"))
+if marks>=90 and marks<=100:
+    print("A")
+elif marks>=89 and marks<=75:
+    print("B")
+elif marks>=74 and marks<=60:
+    print("C")
+elif marks>=59 and marks<=40:
+    print("D")
+elif marks<=40:
+    print("F")
